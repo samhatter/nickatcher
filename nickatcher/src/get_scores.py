2610dc8ba67cbd4f0e01bb7c507e8ffbc39716bb
samhatter <sammyvincent2001@gmail.com>
2025-05-19 18:22:14 -0700

    generating embeddings over more ppl

--- nickatcher/src/get_scores.py
+++ nickatcher/src/get_scores.py
@@ -6,7 +6,7 @@ import numpy as np
 from sklearn.metrics import pairwise_kernels
 from sklearn.decomposition import PCA
 from sklearn.metrics.pairwise import cosine_similarity 
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import normalize
 from sqlalchemy.ext.asyncio import AsyncSession
 
 logger = logging.getLogger('nickatcher')
@@ -34,66 +34,52 @@ async def get_scores(slskd_client: SLSKDClient, session: AsyncSession, room_name
         X = user_embeddings_1.detach().cpu().numpy()
         Y = user_embeddings_2.detach().cpu().numpy()
         
-        X_transformed, Y_transformed = pca(X, Y)
+        X_transformed, Y_transformed = await pca(X, Y, max_tokens, session=session)
         cX, cY = center(X_transformed,Y_transformed)
-        score = euclidean_distance(cX, cY)
+        nX, nY = norm(cX, cY)
+        score = euclidean_distance(nX, nY)
         output_msg = f"Similarity for {user_1}, {user_2}: {str(score)[:5]}. Computed from {num_tokens_1} and {num_tokens_2} tokens respectively. Ranges from (0 uncorrelated to 1 correlated)."
         logger.debug(output_msg)
         await slskd_client.send_message(room_name=room_name, message=output_msg)
 
-def pca(X, Y, variance_threshold=0.95):
-  samples = np.concatenate((X, Y), axis=0)
-  scaler = StandardScaler()
-  scalar_samples = scaler.fit_transform(samples)
-  pca = PCA(svd_solver='full').fit(scalar_samples)
+async def pca(X, Y, max_tokens, session, variance_threshold=0.95):
+  all_messages = await list_messages(session=session, limit=10000)
+  all_embeddings, _ = get_embeddings(list(all_messages), max_tokens=max_tokens)
+  samples = all_embeddings.detach().cpu().numpy()
+  pca = PCA(svd_solver='full').fit(samples)
   
   cumulative_variance = np.cumsum(pca.explained_variance_ratio_)
   optimal_components = np.searchsorted(cumulative_variance, variance_threshold) + 1
   logger.debug("Optimal PCA inner dim chosen: %i", optimal_components)
 
-  transformed_samples = PCA(n_components=optimal_components, svd_solver='full').fit_transform(scalar_samples)
-
-  X_transformed = transformed_samples[:X.shape[0]]
-  Y_transformed = transformed_samples[X.shape[0]:]
+  X_transformed = pca.transform(X)[:,:optimal_components]
+  Y_transformed = pca.transform(Y)[:,:optimal_components]
+  logger.debug("After PCA:")
+  vector_stats(X_transformed, Y_transformed)
 
   return X_transformed, Y_transformed
+
+def vector_stats(X, Y):
+  dot = np.dot(X, Y.T)[0, 0]
+  norm_x = np.linalg.norm(X)
+  norm_y = np.linalg.norm(Y)
+  similarity = dot / (norm_x * norm_y)
+  logger.debug("Dot: %.5f, NormX: %.5f, NormY: %.5f, CosSim: %.5f", dot, norm_x, norm_y, similarity)
 
 def center(X, Y):
   cX = X.mean(axis=0, keepdims=True)
   cY = Y.mean(axis=0, keepdims=True)
-  dot = np.dot(cX, cY.T)[0, 0]
-  norm_x = np.linalg.norm(cX)
-  norm_y = np.linalg.norm(cY)
-  similarity = dot / (norm_x * norm_y)
-  logger.debug("Dot: %.5f, NormX: %.5f, NormY: %.5f, CosSim: %.5f", dot, norm_x, norm_y, similarity)
+  logger.debug("After centering:")
+  vector_stats(cX, cY)
   return cX, cY
 
-def euclidean_distance(cX, cY):
-  dist = np.linalg.norm(cX - cY)
+def norm(X, Y):
+  nX = normalize(X, 'l2')
+  nY = normalize(Y, 'l2')
+  logger.debug("After normalizing:")
+  vector_stats(nX, nY)
+  return nX, nY
+
+def euclidean_distance(X, Y):
+  dist = np.linalg.norm(X - Y)
   return float(1/(1+dist))
-
-def cosine(cX, cY):
-  similarity = cosine_similarity(cX, cY)[0, 0]
-  return float(similarity)
-
-def mmd_rbf(user_embeddings_1, user_embeddings_2, gamma=1.0):
-    """
-    Calculates the squared MMD using an RBF (Gaussian) kernel.
-    
-    Args:
-      X: A numpy array representing the first sample (n_samples1, n_features).
-      Y: A numpy array representing the second sample (n_samples2, n_features).
-      gamma: The kernel coefficient for RBF kernel.
-    
-    Returns:
-      The squared MMD value.
-    """
-
-    X = user_embeddings_1.detach().cpu().numpy()
-    Y = user_embeddings_2.detach().cpu().numpy()
-
-    k_xx = pairwise_kernels(X, X, metric='rbf', gamma=gamma)
-    k_yy = pairwise_kernels(Y, Y, metric='rbf', gamma=gamma)
-    k_xy = pairwise_kernels(X, Y, metric='rbf', gamma=gamma)
-    
-    return np.mean(k_xx) + np.mean(k_yy) - 2 * np.mean(k_xy)
